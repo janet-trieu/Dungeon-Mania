7211366b7ea7fd51d47735433f7c3483036052dc
Janet Trieu <janet.trieu@outlook.com>
2021-10-24 00:39:06 +1100

    Added testSwitchBoulder, tests if boulder moves and floorswitch activates

--- src/test/java/dungeonmania/StaticTest.java
+++ src/test/java/dungeonmania/StaticTest.java
@@ -15,68 +15,113 @@ import dungeonmania.util.Position;
 
 /**
  * Testing for static entity functionality:
- *      - Portals
- *      - Wall, Exit
- *      - Boulder
- *      - FloorSwitch
- *      - Door
- *      - ZombieToastSpawner
+ *      - Portal
+ *      - Boulder, FloorSwitch
+ *      - Exit, Door (+ Key)
+ *      - Wall, ZombieToastSpawner
  */
 public class StaticTest {
 
     /**
      * Testing portal functionality
+     * - Player must teleport to corresponding portal and teleport to cardinally adjacent cell
      * @throws IOException
      */
     @Test
     public void testPortals() throws IOException {
         DungeonManiaController controller = new DungeonManiaController();
 
-        String path = FileLoader.loadResourceFile("/dungeons/portals.json");
-        controller.newGame(path, "Standard");
+        String map = FileLoader.loadResourceFile("/dungeons/portals.json");
+        controller.newGame(map, "Standard");
 
         // CASE: TELEPORT RIGHT
         // Player: (0,0) -> (5,0)
         controller.tick("", Direction.RIGHT);
         Position playerPosition= new Position(5, 0);
-        assertEquals(new EntityResponse("player0", "player", playerPosition, false), controller.getInfo("player0"));
+        assertEquals(new EntityResponse("Player0", "Player", playerPosition, false), controller.getInfo("Player0"));
 
         // CASE: TELEPORT RIGHT
         // Player: (5,0) -> (0,0)
         controller.tick("", Direction.LEFT);
         playerPosition= new Position(0, 0);
-        assertEquals(new EntityResponse("player0", "player", playerPosition, false), controller.getInfo("player0"));
+        assertEquals(new EntityResponse("Player0", "Player", playerPosition, false), controller.getInfo("Player0"));
 
         // MOVING TO SET UP NEXT CASES
         controller.tick("", Direction.RIGHT);
         playerPosition= new Position(5, 0);
-        assertEquals(new EntityResponse("player0", "player", playerPosition, false), controller.getInfo("player0"));
+        assertEquals(new EntityResponse("Player0", "Player", playerPosition, false), controller.getInfo("Player0"));
         controller.tick("", Direction.DOWN);
         playerPosition = new Position(5, 1);
-        assertEquals(new EntityResponse("player0", "player", playerPosition, false), controller.getInfo("player0"));
+        assertEquals(new EntityResponse("Player0", "Player", playerPosition, false), controller.getInfo("Player0"));
         controller.tick("", Direction.LEFT);
         playerPosition = new Position(4, 1);
-        assertEquals(new EntityResponse("player0", "player", playerPosition, false), controller.getInfo("player0"));
+        assertEquals(new EntityResponse("Player0", "Player", playerPosition, false), controller.getInfo("Player0"));
 
         // CASE: TELEPORT UP
         // Player: (4,1) -> (1, -1)
         controller.tick("", Direction.UP);
         playerPosition = new Position(1, -1);
-        assertEquals(new EntityResponse("player0", "player", playerPosition, false), controller.getInfo("player0"));
+        assertEquals(new EntityResponse("Player0", "Player", playerPosition, false), controller.getInfo("Player0"));
 
         // CASE: TELEPORT DOWN
         // Player: (1,-1) -> (4,1)
         controller.tick("", Direction.DOWN);
         playerPosition = new Position(4, 1);
-        assertEquals(new EntityResponse("player0", "player", playerPosition, false), controller.getInfo("player0"));
+        assertEquals(new EntityResponse("Player0", "Player", playerPosition, false), controller.getInfo("Player0"));
     }
 
     /**
-     * Testing 'Wall' and 'Exit' functionality
+     * Testing boulder and switch functionality
+     * - If pushing two Boulder(s), stay put
+     * - If Boulder on the same cell as FloorSwitch, it activates it
+     * @throws IOException
      */
     @Test
-    public void testMaze() {
+    public void testSwitchBoulder() throws IOException {
         DungeonManiaController controller = new DungeonManiaController();
+
+        String map = FileLoader.loadResourceFile("/dungeons/StaticTest/simple-switch-boulder.json");
+        controller.newGame(map, "Standard");
+
+        // CASE: PLAYER CANNOT PUSH 2 BOULDER(S) AT ONCE
+        controller.tick("", Direction.DOWN);
+        // Ensure Player, Boulder(s) are still in place
+        Position position = new Position(0, 0);
+        assertEquals(new EntityResponse("Player0", "Player", position, false), controller.getInfo("Player0"));
+        position = new Position(0, 1);
+        assertEquals(new EntityResponse("Boulder0", "Boulder", position, false), controller.getInfo("Boulder0"));
+        position = new Position(0, 2);
+        assertEquals(new EntityResponse("Boulder1", "Boulder", position, false), controller.getInfo("Boulder1"));
+
+        // CASE: PUSH BOULDER ONTO FLOORSWITCH ACTIVATES IT
+        // Push Boulder0 to FloorSwitch0
+        controller.tick("", Direction.RIGHT);
+        controller.tick("", Direction.DOWN);
+        controller.tick("", Direction.LEFT);
+        position = new Position(0, 1);
+        assertEquals(new EntityResponse("Player0", "Player", position, false), controller.getInfo("Player0"));
+        position = new Position(-1, 1);
+        assertEquals(new EntityResponse("Boulder0", "Boulder", position, false), controller.getInfo("Boulder0"));
+        // ENSURE FLOORSWITCH0 IS ACTIVATED - not sure how to check this at the moment :(
         
+        // Push Boulder1 to FloorSwitch1
+        controller.tick("", Direction.RIGHT);
+        controller.tick("", Direction.DOWN);
+        controller.tick("", Direction.DOWN);
+        controller.tick("", Direction.LEFT);
+        controller.tick("", Direction.LEFT);
+        controller.tick("", Direction.UP);
+        controller.tick("", Direction.RIGHT);
+        position = new Position(0, 2);
+        assertEquals(new EntityResponse("Player0", "Player", position, false), controller.getInfo("Player0"));
+        position = new Position(1, 2);
+        assertEquals(new EntityResponse("Boulder1", "Boulder", position, false), controller.getInfo("Boulder0"));
+        // ENSURE FLOORSWITCH1 IS ACTIVATED - not sure how to check this at the moment :(
     }
+
+    @Test
+    public void testDoorExit() {
+
+    }
+
 }
