cf4cdbbe05661f10ad1c3adb1f71c9f5897d9dce
unknown <Francis Nguyen>
2021-10-25 22:35:11 +1100

    created file for tests for inventory features

---
+++ src/test/java/dungeonmania/InventoryTest.java
@@ -0,0 +1,15 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.FileLoader;
+import dungeonmania.util.Position;
+
+public class InventoryTest {
+    
+}
