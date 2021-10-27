dfa33afa9fc720c3fb075bd6d68edc7ed718c74d
unknown <Francis Nguyen>
2021-10-28 10:03:50 +1100

    added attributes and methods to player, Entity, Dungeon, and Goal

--- src/main/java/dungeonmania/goals/SwitchGoal.java
+++ src/main/java/dungeonmania/goals/SwitchGoal.java
@@ -1,5 +1,7 @@
 package dungeonmania.goals;
 
 public class SwitchGoal extends Goal {
-    
+    public SwitchGoal() {
+        // TODO
+    }
 }
--- src/main/java/dungeonmania/goals/TreasureGoal.java
+++ src/main/java/dungeonmania/goals/TreasureGoal.java
@@ -1,5 +1,7 @@
 package dungeonmania.goals;
 
 public class TreasureGoal extends Goal {
-    
+    public TreasureGoal() {
+        // TODO
+    }
 }
