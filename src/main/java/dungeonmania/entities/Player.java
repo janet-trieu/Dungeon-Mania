a404b6036df779c8b1f038f141b1e853227cdcac
James Nguyen <james.v.nguyen@student.unsw.edu.au>
2021-10-28 20:15:06 +1100

    Finished simple implementation of move function and player move methods

--- src/main/java/dungeonmania/entities/Player.java
+++ src/main/java/dungeonmania/entities/Player.java
@@ -1,8 +1,9 @@
 package dungeonmania.entities;
 
 import dungeonmania.entities.movingEntity.MovingEntity;
+import dungeonmania.util.Direction;
 
-public class Player extends Entity {
+public class Player extends Entity implements Moveable {
     private int health;
     private int maxHealth;
     private PotionState potionState;
@@ -21,17 +22,8 @@ public class Player extends Entity {
         // TODO
     }
 
-    public void moveUp() {
-        // TODO
-    }
-
-    public void moveDown() {
-        // TODO
-    }
-    public void moveLeft() {
-        // TODO
-    }
-    public void moveRight() {
-        // TODO
-    }
+    public void moveUp() {move(Direction.UP, this);}
+    public void moveDown() {move(Direction.DOWN, this);}
+    public void moveLeft() {move(Direction.LEFT, this);}
+    public void moveRight() {move(Direction.RIGHT, this);}
 }
