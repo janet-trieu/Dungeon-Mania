81e150be8eb2ec331a404da6b4f3e8ce7ed6a72c
James Nguyen <james.v.nguyen@student.unsw.edu.au>
2021-10-29 21:16:21 +1100

    Implementation of spider and zombietoast movement, without boulder checking

--- src/main/java/dungeonmania/entities/movingEntity/Spider.java
+++ src/main/java/dungeonmania/entities/movingEntity/Spider.java
@@ -1,14 +1,46 @@
 package dungeonmania.entities.movingEntity;
+import java.util.List;
 
-public class Spider extends MovingEntity {
+import dungeonmania.util.*;
+
+public class Spider extends MovingEntity implements Moveable {
     
-    private int movementCounter;
+    private int movementCounter = 0;
+    private String way = "Right";
 
     public Spider(int x, int y) {
         super(x, y, "spider", 1, 10);
     }
 
     public void move() {
-        
+        if(movementCounter == 0) {
+            move(Direction.UP, this);
+            increment();
+        }
+
+        if(way == "Right") {
+            if(movementCounter == 1 || movementCounter == 8) {move(Direction.RIGHT, this);}
+            else if(movementCounter == 2 || movementCounter == 3) {move(Direction.DOWN, this);}
+            else if(movementCounter == 4 || movementCounter == 5) {move(Direction.LEFT, this);}
+            else if(movementCounter == 6 || movementCounter == 7) {move(Direction.UP,this);}
+            increment();
+        }
+
+        if(way == "Left") {
+            if(movementCounter == 5 || movementCounter == 6) {move(Direction.RIGHT, this);}
+            else if(movementCounter == 7 || movementCounter == 8) {move(Direction.DOWN, this);}
+            else if(movementCounter == 1 || movementCounter == 2) {move(Direction.LEFT, this);}
+            else if(movementCounter == 3 || movementCounter == 4) {move(Direction.UP,this);}
+            increment();
+        }
+
+    }
+
+    public void increment() {
+        if (movementCounter == 8) {
+            this.movementCounter = 1;
+            return;
+        }
+        this.movementCounter = movementCounter + 1;
     }
 }
--- src/main/java/dungeonmania/entities/movingEntity/ZombieToast.java
+++ src/main/java/dungeonmania/entities/movingEntity/ZombieToast.java
@@ -1,6 +1,8 @@
 package dungeonmania.entities.movingEntity;
 
-public class ZombieToast extends MovingEntity {
+import dungeonmania.util.Direction;
+
+public class ZombieToast extends MovingEntity implements Moveable {
     
     Boolean hasArmour;
 
@@ -9,14 +11,19 @@ public class ZombieToast extends MovingEntity {
     }
 
     public void move() {
-        
+        int way = randomise();
+        if(way == 1) {move(Direction.UP, this);}
+        if(way == 2) {move(Direction.DOWN, this);}
+        if(way == 3) {move(Direction.LEFT, this);}
+        if(way == 4) {move(Direction.RIGHT, this);}
     }
 
     public void dropArmour() {
 
     }
 
-    public void randomise() {
-
+    public int randomise() {
+        int random = (int)(Math.random()*4) + 1;
+        return random;
     }
 }
