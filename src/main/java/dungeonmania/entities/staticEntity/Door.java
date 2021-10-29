84697595f7ad1ad88e7fb006ed3a383bb741527c
Janet Trieu <janet.trieu@outlook.com>
2021-10-29 15:27:25 +1100

    Altered constructors + added attributes to suit JSON maps and UML

--- src/main/java/dungeonmania/entities/staticEntity/Door.java
+++ src/main/java/dungeonmania/entities/staticEntity/Door.java
@@ -1,14 +1,34 @@
 package dungeonmania.entities.staticEntity;
 
-public abstract class Door extends StaticEntity {
+public class Door extends StaticEntity {
 
     // storing the number of entities created to help with fluid entityId generation
     private static int counter = 0;
+    private int key;
 
-    public Door(int x, int y) {
+    public Door(int x, int y, int key) {
         super(x, y, "door");
         setId("door" + String.valueOf(counter));
         counter++;
+        setKey(key);
     }
 
+    public Door(int x, int y, Boolean isPassable) {
+        super(x, y, "door");
+        setId("door" + String.valueOf(counter));
+        counter++;
+        setPassable(isPassable);
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
 }
