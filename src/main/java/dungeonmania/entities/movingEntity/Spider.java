a78d5f82787d72596140fd66dbf21b4379511162
James Nguyen <james.v.nguyen@student.unsw.edu.au>
2021-10-29 17:20:14 +1100

    Initial implementation of moving entities class files

--- src/main/java/dungeonmania/entities/movingEntity/Spider.java
+++ src/main/java/dungeonmania/entities/movingEntity/Spider.java
@@ -2,4 +2,10 @@ package dungeonmania.entities.movingEntity;
 
 public class Spider extends MovingEntity {
     
+    private int movementCounter;
+
+    public Spider(int x, int y) {
+        super(x, y, "spider", 1, 10);
+    }
+
 }
--- src/main/java/dungeonmania/entities/movingEntity/ZombieToast.java
+++ src/main/java/dungeonmania/entities/movingEntity/ZombieToast.java
@@ -2,4 +2,21 @@ package dungeonmania.entities.movingEntity;
 
 public class ZombieToast extends MovingEntity {
     
+    Boolean hasArmour;
+
+    public ZombieToast(int x, int y) {
+        super(x, y, "zombie_toast", 5, 5);
+    }
+
+    public void move() {
+        
+    }
+
+    public void dropArmour() {
+
+    }
+
+    public void randomise() {
+
+    }
 }
